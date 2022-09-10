8de45fb8ed77d111162c34f33c9f5f2140abff93
akbarob <akbarbadmus07@gmail.com>
2022-09-10 14:17:46 +0100

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7h2a0ejTSQUTg8gl93_5VJYzHuzSMZek",
+    authDomain: "wears-app.firebaseapp.com",
+    projectId: "wears-app",
+    storageBucket: "wears-app.appspot.com",
+    messagingSenderId: "441432252811",
+    appId: "1:441432252811:web:3f765f70177f68051d0f8e",
+    measurementId: "G-0YC5NJPZJZ"
+  }
